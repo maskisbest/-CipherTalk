483ae3a36832721a95947617b33509120f176315
LittFlower <xzy1476569428@163.com>
2025-07-17 08:37:52 +0800

    Add login and register logic.

---
+++ hvigorfile.ts
@@ -0,0 +1,12 @@
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: [], /* Custom plugin to extend the functionality of Hvigor. */
+  // modules: [
+  //   {
+  //     name: 'ohosFramework',
+  //     path: 'ohosFramework'
+  //   }
+  // ]
+}
